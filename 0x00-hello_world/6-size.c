81228a44ccadcc6a936ffb1d6f4b0fef0fe6eb63
johnowosho <owoshojohn1000@gmail.com>
2023-02-21 13:40:08 -0800

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 * Return: Always 0 (Success)
 */
 {
-printf("Size of a char: %d bytes(s)\n", sizeof(char);
-printf("Size of an int: %d byte(s)\n", sizeof(int);
-printf("Size of a lond int: %d byte(s)\n", sizeof(long int);
-printf("Size od a long long int: %d byte(s)\n", sizeof(long long iny);
-printf("Size of a float: %d byte(s)\n", sizeof(float);
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a lond int: %d byte(s)\n", sizeof(long int));
+printf("Size od a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
